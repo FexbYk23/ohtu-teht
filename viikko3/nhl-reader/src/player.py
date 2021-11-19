5eecdea41cf02055ceb71fc696a0e3ce69f56223
FexbYk <>
2021-11-19 14:26:00 +0200

    viikko3 t1

--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,12 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, assists, goals, penalties, team, games):
         self.name = name
+        self.assists = assists
+        self.goals = goals
+        self.penalties = penalties
+        self.games = games
+        self.nationality = nationality
+        self.team = team
     
     def __str__(self):
-        return self.name
+        return self.name + " team: " + self.team + f" goals: {self.goals}"

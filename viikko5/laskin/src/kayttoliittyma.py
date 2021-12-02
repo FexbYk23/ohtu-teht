0eb7a991f6c87072d84267f337ac794d14a42756
FexbYk <>
2021-12-02 13:08:18 +0200

    viikko5 t5

--- viikko5/laskin/src/kayttoliittyma.py
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -12,9 +12,14 @@ class Summa:
     def __init__(self, sovellus, lue):
         self.sovellus = sovellus
         self.lue = lue
-
+        
     def suorita(self):
-        self.sovellus.plus(self.lue())
+        self.arvo = self.lue()
+        self.sovellus.plus(self.arvo)
+
+    def kumoa(self):
+        self.sovellus.miinus(self.arvo)
+        
 
 class Erotus:
     def __init__(self, sovellus, lue):
@@ -22,25 +27,45 @@ class Erotus:
         self.lue = lue
 
     def suorita(self):
-        self.sovellus.miinus(self.lue())
+        self.arvo = self.lue()
+        self.sovellus.miinus(self.arvo)
+
+    def kumoa(self):
+        self.sovellus.plus(self.arvo)
 
 class Nollaus:
     def __init__(self, sovellus):
         self.sovellus = sovellus
 
     def suorita(self):
+        self.arvo = self.sovellus.tulos
         self.sovellus.nollaa()
 
+    def kumoa(self):
+        self.sovellus.aseta_arvo(self.arvo)
+
+
+class Kumoa:
+    def __init__(self, lue_komento):
+        self.lue_komento = lue_komento
+
+    def suorita(self):
+        self.lue_komento().kumoa()
+
+    def kumoa(self):
+        pass
 
 class Kayttoliittyma:
     def __init__(self, sovellus, root):
         self._sovellus = sovellus
         self._root = root
+        self._viimeisin_komento = None
 
         self.__komennot = {
             Komento.SUMMA : Summa(self._sovellus, self._lue_syote),
             Komento.EROTUS : Erotus(self._sovellus, self._lue_syote),
             Komento.NOLLAUS : Nollaus(self._sovellus),
+            Komento.KUMOA : Kumoa(self._lue_kumottava_komento)
         }
 
     def kaynnista(self):
@@ -86,6 +111,8 @@ class Kayttoliittyma:
     def _lue_syote(self):
         return int(self._syote_kentta.get())
 
+    def _lue_kumottava_komento(self):
+        return self._viimeisin_komento
 
     def _suorita_komento(self, komento):
         arvo = 0
@@ -96,8 +123,12 @@ class Kayttoliittyma:
             pass
 
         self.__komennot[komento].suorita()
+        self._viimeisin_komento = self.__komennot[komento]
 
-        self._kumoa_painike["state"] = constants.NORMAL
+        if komento == Komento.KUMOA:
+            self._kumoa_painike["state"] = constants.DISABLED
+        else:
+            self._kumoa_painike["state"] = constants.NORMAL
 
         if self._sovellus.tulos == 0:
             self._nollaus_painike["state"] = constants.DISABLED
